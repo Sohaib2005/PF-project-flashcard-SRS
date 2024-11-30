9f2baf4a223b7b83caafb32d92f32d3c21748f0f
Muhammad Sohaib <164145499+Sohaib2005@users.noreply.github.com>
2024-12-01 00:27:30 +0500

    defined other functions
    functions defined:
    edit flashcard
    study flashcard
    display flashcard

--- test/test.c
+++ test/test.c
@@ -73,3 +73,75 @@ void deleteFlashcard(Flashcard** cards, int* count)
         return;
     }
 }
+void editFlashcard(Flashcard* cards, int count)
+{
+    int id,correct;
+    printf("\nEnter id of flash card you want to edit: ");
+    scanf("%d",&id);
+    correct=validate(id,&count,&cards);
+    if(correct)
+    {
+        printf("Question: %s\n",cards[j].question);
+        printf("Answer : %s\n\n",cards[j].answer);
+        getchar();
+        printf("Edit Question: ");
+        fgets(cards[j].question,256,stdin);
+        cards[j].question[strcspn(cards[j].question, "\n")] = '\0';
+        printf("Edit Answer: ");
+        fgets(cards[j].answer,256,stdin);
+        cards[j].answer[strcspn(cards[j].answer, "\n")] = '\0';
+        printf("Flashcard edited successfully!\n");
+    }
+    else
+    {
+        printf("ID does not exist!\n");
+        return;
+    }
+}
+void studyFlashcards(Flashcard* cards, int count)
+{
+    long now = time(NULL);
+    for (int i = 0; i < count; i++) {
+        if (cards[i].nextReview <= now) {
+            printf("\nQuestion: %s\n", cards[i].question);
+            char userAnswer[256];
+            printf("Your Answer: ");
+            fgets(userAnswer, 256, stdin);
+            userAnswer[strcspn(userAnswer, "\n")] = '\0';
+
+            printf("Correct Answer: %s\n", cards[i].answer);
+            printf("Rate your response (0-5): ");
+            int quality;
+            scanf("%d", &quality);
+            getchar();
+
+            if (quality >= 3) {
+                if (cards[i].repetitions == 0) {
+                    cards[i].interval = 1;
+                } else if (cards[i].repetitions == 1) {
+                    cards[i].interval = 6;
+                } else {
+                    cards[i].interval *= cards[i].easinessFactor;
+                }
+                cards[i].repetitions++;
+            } else {
+                cards[i].repetitions = 0;
+                cards[i].interval = 1;
+            }
+
+            cards[i].easinessFactor += (0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02));
+            if (cards[i].easinessFactor < 1.3) cards[i].easinessFactor = 1.3;
+
+            cards[i].nextReview = now + cards[i].interval * 24 * 3600;
+            }
+            }
+}
+void display(Flashcard* cards,int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        printf("\n%d. ", cards[i].id);
+        printf("%s \n", cards[i].question);
+        printf("   %s \n", cards[i].answer);
+    }
+}
